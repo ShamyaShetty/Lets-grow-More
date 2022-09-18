a60c26099e9b452bc76701270839579b95fc4b75
ShamyaShetty <92239580+ShamyaShetty@users.noreply.github.com>
2022-09-18 08:28:30 +0530

    Update calsi.js

--- calcu/calsi.js
+++ calcu/calsi.js
@@ -1,7 +1,3 @@
-console.log(
-    "Javascript Calculator Made by Harsh Trivedi\nhttps://harsh98trivedi.github.io"
-);
-
 document.getElementById("answer").readOnly = true; //set this attribute in Html file
 let screen = document.getElementById("answer");
 buttons = document.querySelectorAll("button");
